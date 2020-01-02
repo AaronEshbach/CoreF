039e21723c9aef4f48452da946c6b735e3d9ab05
Aaron Eshbach <aaron.eshbach@hotmail.com>
2020-01-02 10:10:42 -0500

    Added HttpApiClient to simplify usage with DTOs and Models.

---
+++ CoreF/CoreF.Http.Caching/Cache.fs
@@ -0,0 +1,21 @@
+﻿namespace CoreF.Http.Caching
+
+open CoreF.DependencyInjection
+open CoreF.Serialization
+open Microsoft.Extensions.Caching.Distributed
+
+module Cache =
+    let tryFind<'key, 'value when 'key :> IKey> (key: 'key) =
+        injectedAsync {
+            let! cache = inject<IDistributedCache>() |> Injected.mapError DependencyInjectionError
+            let! serializer = inject<ISerializer>() |> Injected.mapError DependencyInjectionError
+            let! cacheData = cache.GetAsync(key.ToCacheKey()) |> Async.AwaitTask
+            match cacheData |> Option.ofObj with
+            | Some [||] -> 
+                return None
+            | Some bytes ->
+                let! value = bytes |> serializer.Deserialize<'value> |> Result.mapError SerializationError
+                return Some value
+            | None ->
+                return None
+        }
---
+++ CoreF/CoreF.Http.Caching/Model.fs
@@ -0,0 +1,13 @@
+﻿namespace CoreF.Http.Caching
+
+open CoreF.DependencyInjection
+open CoreF.Serialization
+
+type HttpCacheError =
+| DependencyInjectionError of DependencyInjectionError
+| SerializationError of SerializationError
+| UnexpectedCacheError of exn
+
+type IKey =
+    abstract member ToCacheKey: unit -> string
+
