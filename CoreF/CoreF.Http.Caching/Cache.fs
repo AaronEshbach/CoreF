abadae3947973a6b99949d76ef326117b3195ad7
Aaron Eshbach <aaron.eshbach@hotmail.com>
2020-01-06 09:14:07 -0500

    Implemented basic getOrAdd functionality for cache.

--- CoreF/CoreF.Http.Caching/Cache.fs
+++ CoreF/CoreF.Http.Caching/Cache.fs
@@ -5,11 +5,11 @@ open CoreF.Serialization
 open Microsoft.Extensions.Caching.Distributed
 
 module Cache =
-    let tryFind<'key, 'value when 'key :> IKey> (key: 'key) =
+    let tryFind<'value> (key: CacheKey) =
         injectedAsync {
             let! cache = inject<IDistributedCache>() |> Injected.mapError DependencyInjectionError
             let! serializer = inject<ISerializer>() |> Injected.mapError DependencyInjectionError
-            let! cacheData = cache.GetAsync(key.ToCacheKey()) |> Async.AwaitTask
+            let! cacheData = key |> CacheKey.value |> cache.GetAsync |> Async.AwaitTask
             match cacheData |> Option.ofObj with
             | Some [||] -> 
                 return None
@@ -18,4 +18,24 @@ module Cache =
                 return Some value
             | None ->
                 return None
+        }
+
+    let add<'value> (key: CacheKey) (value: 'value) =
+        injectedAsync {
+            let! cache = inject<IDistributedCache>() |> Injected.mapError DependencyInjectionError
+            let! serializer = inject<ISerializer>() |> Injected.mapError DependencyInjectionError
+            let! data = serializer.Serialize(value) |> Result.mapError SerializationError
+            let cacheKey = key |> CacheKey.value
+            return! cache.SetAsync(cacheKey, data) |> Async.AwaitTask
+        }
+
+    let getOrAdd<'value> (key: CacheKey) (f: unit -> Async<'value>) =
+        injectedAsync {
+            match! key |> tryFind<'value> with
+            | Some value ->
+                return value
+            | None ->
+                let! value = f ()
+                do! value |> add<'value> key
+                return value
         }
--- CoreF/CoreF.Http.Caching/Model.fs
+++ CoreF/CoreF.Http.Caching/Model.fs
@@ -1,13 +1,35 @@
 ﻿namespace CoreF.Http.Caching
 
+open CoreF.Common
 open CoreF.DependencyInjection
 open CoreF.Serialization
 
 type HttpCacheError =
 | DependencyInjectionError of DependencyInjectionError
 | SerializationError of SerializationError
+| CacheKeyCannotBeEmpty
+| KeyGeneratorNotFound of System.Type
 | UnexpectedCacheError of exn
 
-type IKey =
-    abstract member ToCacheKey: unit -> string
+[<Struct>]
+type CacheKey = private CacheKey of string
+
+type IKeyGenerator<'model> =
+    abstract member GetKey : 'model -> CacheKey
+
+module CacheKey =   
+    let create (key: string) =
+        if key |> isNullOrEmpty then
+            Error CacheKeyCannotBeEmpty
+        else
+            Ok <| CacheKey key
+
+    let generate<'t> (value: 't) =
+        injected {
+            let! generator = inject<IKeyGenerator<'t>>()
+            return generator.GetKey value
+        } |> Injected.mapError (fun _ -> KeyGeneratorNotFound typeof<'t>)
+
+    let value (CacheKey key) = key
+
 
