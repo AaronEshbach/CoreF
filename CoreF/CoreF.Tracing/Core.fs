ec50f20d8900a36544c4d2e6995c8c77d2960b35
Aaron Eshbach <aaron.eshbach@hotmail.com>
2020-01-15 13:53:07 -0500

    Added CoreF.Tracing project.

---
+++ CoreF/CoreF.Tracing/Core.fs
@@ -0,0 +1,19 @@
+﻿namespace CoreF.Tracing
+
+open OpenTracing
+
+type TracingError =
+| OpenTracingNotConfigured
+| InvalidTraceProviderType of System.Type
+| UnexpectedTracingError of exn
+
+type TraceLevel =
+| Verbose
+| Detailed
+| Normal
+| Minimal
+
+type IDistributedTracing =
+    abstract member Tracer: ITracer
+    abstract member Enabled: bool
+    abstract member Level: TraceLevel
