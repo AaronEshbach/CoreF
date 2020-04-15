a1913a2a8d5f8667276c04d37eaf487526818eba
Aaron Eshbach <aaron.eshbach@hotmail.com>
2020-04-15 08:59:11 -0400

    Added more complete features for caching HTTP requests.

--- CoreF/CoreF.Http.Caching/Model.fs
+++ CoreF/CoreF.Http.Caching/Model.fs
@@ -2,22 +2,37 @@
 
 open CoreF.Common
 open CoreF.DependencyInjection
+open CoreF.Http
 open CoreF.Serialization
+open System
+open System.Security.Cryptography
 
 type HttpCacheError =
 | DependencyInjectionError of DependencyInjectionError
 | SerializationError of SerializationError
 | CacheKeyCannotBeEmpty
 | KeyGeneratorNotFound of System.Type
+| PolicyManagerNotFound of System.Type
 | UnexpectedCacheError of exn
 
 [<Struct>]
 type CacheKey = private CacheKey of string
 
 type IKeyGenerator<'model> =
-    abstract member GetKey : 'model -> CacheKey
+    abstract member GetKey : 'model -> Result<CacheKey, HttpCacheError>
 
-type ICacheManager = interface end
+type CacheExpirationPolicy =
+| AbsoluteExpiration of TimeSpan
+| SlidingExpiration of TimeSpan
+| NoExpiration
+
+type ICachePolicyManager<'model> =
+    abstract member GetPolicy : 'model -> Result<CacheExpirationPolicy, HttpCacheError>
+
+type ICachableHttpRequest =
+    inherit IHttpRequest
+    abstract member CachePolicy: CacheExpirationPolicy
+    abstract member CacheKey: CacheKey
 
 module CacheKey =   
     let create (key: string) =
@@ -28,9 +43,24 @@ module CacheKey =
 
     let generate<'t> (value: 't) =
         injected {
-            let! generator = inject<IKeyGenerator<'t>>()
-            return generator.GetKey value
-        } |> Injected.mapError (fun _ -> KeyGeneratorNotFound typeof<'t>)
+            let! generator = inject<IKeyGenerator<'t>>() |> Injected.mapError (fun _ -> KeyGeneratorNotFound typeof<'t>)
+            return! generator.GetKey value
+        } 
 
     let value (CacheKey key) = key
 
+
+type HttpRequestCacheKeyGenerator () =
+    let filterHeaders = Seq.filter (fun (key: string, _) -> key.StartsWith("x-coref-"))
+    
+    member __.Generate (request: IHttpRequest) =
+        let url = request.Url.AbsoluteUri |> String.lowercase
+        let headers = request.Headers |> Map.toSeq |> Seq.map (fun (key, value) -> key |> CaseInsensitiveString.value, value) |> filterHeaders |> Seq.map (fun (key, value) -> sprintf "%s=%s" key value) |> String.join ";"
+        let method = request.Method |> Union.getName |> String.lowercase
+        use sha = new SHA256CryptoServiceProvider() 
+        let requestKey = headers |> Utf8.toBytes |> Array.append request.Body |> sha.ComputeHash |> Base64.toString
+        sprintf "%s-%s-%s" method url requestKey |> CacheKey.create
+
+    interface IKeyGenerator<IHttpRequest> with
+        member this.GetKey request = 
+            this.Generate request
